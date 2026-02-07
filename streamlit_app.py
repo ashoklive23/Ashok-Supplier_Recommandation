4185f4e3180be3170e286102f031a5d7be0b7c39
Ashok <ashoklive23@gmail.com>
2026-02-07 22:52:21 +0530

    Force refresh: Update version to 5.1 and sync data

--- streamlit_app.py
+++ streamlit_app.py
@@ -218,4 +218,4 @@ elif selected_part:
 else:
     st.info("Select criteria to begin analysis.")
 
-st.markdown("<div style='text-align: center; color: #475569; padding: 60px;'>PRODUCTION READY | ENTERPRISE PROCUREMENT INTEL</div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center; color: #475569; padding: 60px;'>PRODUCTION READY | ENTERPRISE PROCUREMENT INTEL v5.1</div>", unsafe_allow_html=True)
